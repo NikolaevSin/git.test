e2df639c59f3fef49349e31a5dbdf5141c262060
NikolaevSin <NikolaevSin@ya.ru>
2020-07-10 22:31:00 +0300

    поновой

---
+++ Snake/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            point p1 = new point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+
+            Draw(p1.x, p1.y, p1.sym);
+
+
+            point p2 = new point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+
+            Draw(p2.x, p2.y, p2.sym);
+
+
+            Console.Read();
+
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
