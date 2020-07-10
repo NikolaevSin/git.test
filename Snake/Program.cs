f51c86737cd90600b66d880731d6d2cd5b053ce1
NikolaevSin <NikolaevSin@ya.ru>
2020-07-10 22:42:13 +0300

    Добавлены изменения в класс.

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -14,25 +14,19 @@ namespace Snake
             p1.x = 1;
             p1.y = 3;
             p1.sym = '*';
+            p1.Drow();
 
-            Draw(p1.x, p1.y, p1.sym);
 
 
             point p2 = new point();
             p2.x = 4;
             p2.y = 5;
             p2.sym = '#';
-
-            Draw(p2.x, p2.y, p2.sym);
-
+            p2.Drow();
 
             Console.Read();
 
         }
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+
     }
 }
